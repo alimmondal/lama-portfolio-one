2e7d5e0a0579f30bde4c953be79d5ee0ec38338f
alimmondal <76857187+alimmondal@users.noreply.github.com>
2022-03-12 16:18:51 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import Contact from './components/contact/Contact';
 import Testimonial from './components/testimonial/Testimonial';
 import Works from './components/works/Works';
 import './app.scss';
+import Menu from './components/menu/Menu';
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="section">
         <Intro />
         <Portfolio />
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,45 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './intro.scss';
+import { init } from 'ityped';
 
 export default function Intro() {
-  return <div className="intro" id="intro"></div>;
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ['Developer', 'Designer', 'Content Creator'],
+    });
+  }, []);
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, </h2>
+          <h1>I'm Alim Mondol</h1>
+          <h3>
+            Freelance <span ref={textRef}></span>
+          </h3>
+          <p>
+            I am the master of HTML, CSS and Javascript. I know everything
+            needed to make a website function, efficient. I didn't stop with the
+            web. I went beyond with most popular Javascript framework called
+            React JS. I even know the deployment, server and security. I will
+            give you 100% web solution..
+          </p>
+        </div>
+
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
